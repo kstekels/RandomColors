c58f343d2162847dff82b0826a9a5e00c548c819
Karlis <karlisstekels.dev@gmail.com>
2023-01-30 09:02:03 +0200

    Initial Commit

---
+++ Usefull Color Extensions/Usefull_Color_ExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Usefull_Color_ExtensionsApp.swift
+//  Usefull Color Extensions
+//
+//  Created by karlis.stekels on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Usefull_Color_ExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
